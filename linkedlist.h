2ab10de85e423660057a0d2084c3eb326690599a
Dave Musicant (home machine) <dmusicant@carleton.edu>
2024-01-26 00:03:51 -0600

    Updates for winter 2024

--- linkedlist.h
+++ linkedlist.h
@@ -1,18 +1,18 @@
 #include <stdbool.h>
-#include "value.h"
+#include "object.h"
 
 #ifndef _LINKEDLIST
 #define _LINKEDLIST
 
 // Create a new NULL_TYPE value node.
-Value *makeNull();
+Object *makeNull();
 
 // Create a new CONS_TYPE value node.
-Value *cons(Value *newCar, Value *newCdr);
+Object *cons(Object *newCar, Object *newCdr);
 
 // Display the contents of the linked list to the screen in some kind of
 // readable format
-void display(Value *list);
+void display(Object *list);
 
 // Return a new list that is the reverse of the one that is passed in. All
 // content within the list should be duplicated; there should be no shared
@@ -21,7 +21,7 @@ void display(Value *list);
 // FAQ: What if there are nested lists inside that list?
 // ANS: There won't be for this assignment. There will be later, but that will
 // be after we've got an easier way of managing memory.
-Value *reverse(Value *list);
+Object *reverse(Object *list);
 
 // Frees up all memory directly or indirectly referred to by list. This includes strings.
 //
@@ -35,23 +35,23 @@ Value *reverse(Value *list);
 //
 // ANS: There won't be for this assignment. There will be later, but that will
 // be after we've got an easier way of managing memory.
-void cleanup(Value *list);
+void cleanup(Object *list);
 
 // Utility to make it less typing to get car value. Use assertions to make sure
 // that this is a legitimate operation.
-Value *car(Value *list);
+Object *car(Object *list);
 
 // Utility to make it less typing to get cdr value. Use assertions to make sure
 // that this is a legitimate operation.
-Value *cdr(Value *list);
+Object *cdr(Object *list);
 
 // Utility to check if pointing to a NULL_TYPE value. Use assertions to make sure
 // that this is a legitimate operation.
-bool isNull(Value *value);
+bool isNull(Object *value);
 
 // Measure length of list. Use assertions to make sure that this is a legitimate
 // operation.
-int length(Value *value);
+int length(Object *value);
 
 
 #endif
--- main.c
+++ main.c
@@ -5,95 +5,95 @@
 #include <stdio.h>
 #include <assert.h>
 #include "linkedlist.h"
-#include "value.h"
+#include "object.h"
 
-void testForward(Value *head, int correctLength, bool exemplary) {
-  Value *value = head;
-  assert(CONS_TYPE == value->type);
-  assert(DOUBLE_TYPE == car(value)->type);
-  assert(1 == car(value)->d);
-
-  if (exemplary) {
-    value = cdr(value);
-    assert(CONS_TYPE == value->type);
-    assert(STR_TYPE == car(value)->type);
-    assert(!strcmp("2.0s", car(value)->s));
-
-    value = cdr(value);
-    assert(CONS_TYPE == value->type);
-    assert(STR_TYPE == car(value)->type);
-    assert(!strcmp("3.0s", car(value)->s));
-  }
-
-  value = cdr(value);
-  assert(CONS_TYPE == value->type);
-  assert(DOUBLE_TYPE == car(value)->type);
-  assert(4 == car(value)->d);
+void testForward(Object *head, int correctLength, bool exemplary) {
+  Object *object = head;
+  assert(CONS_TYPE == object->type);
+  assert(DOUBLE_TYPE == car(object)->type);
+  assert(1 == ((Double*)car(object))->value);
 
   if (exemplary) {
-    value = cdr(value);
-    assert(CONS_TYPE == value->type);
-    assert(STR_TYPE == car(value)->type);
-    assert(!strcmp("5.0s", car(value)->s));
+    object = cdr(object);
+    assert(CONS_TYPE == object->type);
+    assert(STR_TYPE == car(object)->type);
+    assert(!strcmp("2.0s", ((String*)car(object))->value));
+
+    object = cdr(object);
+    assert(CONS_TYPE == object->type);
+    assert(STR_TYPE == car(object)->type);
+    assert(!strcmp("3.0s", ((String*)car(object))->value));
   }
 
-  value = cdr(value);
-  assert(CONS_TYPE == value->type);
-  assert(DOUBLE_TYPE == car(value)->type);
-  assert(6 == car(value)->d);
+  object = cdr(object);
+  assert(CONS_TYPE == object->type);
+  assert(DOUBLE_TYPE == car(object)->type);
+  assert(4 == ((Double*)car(object))->value);
 
-  value = cdr(value);
-  assert(CONS_TYPE == value->type);
-  assert(INT_TYPE == car(value)->type);
-  assert(7 == car(value)->i);
+  if (exemplary) {
+    object = cdr(object);
+    assert(CONS_TYPE == object->type);
+    assert(STR_TYPE == car(object)->type);
+    assert(!strcmp("5.0s", ((String*)car(object))->value));
+  }
 
-  value = cdr(value);
-  assert(NULL_TYPE == value->type);
+  object = cdr(object);
+  assert(CONS_TYPE == object->type);
+  assert(DOUBLE_TYPE == car(object)->type);
+  assert(6 == ((Double*)car(object))->value);
+
+  object = cdr(object);
+  assert(CONS_TYPE == object->type);
+  assert(INT_TYPE == car(object)->type);
+  assert(7 == ((Integer*)car(object))->value);
+
+  object = cdr(object);
+  assert(NULL_TYPE == object->type);
 
   assert(correctLength == length(head));
   assert(!isNull(head));
 }
 
-void testBackward(Value *head, int correctLength, bool exemplary) {
-  Value *value = head;
+void testBackward(Object *head, int correctLength, bool exemplary) {
+  Object *value = head;
 
   assert(CONS_TYPE == value->type);
   assert(INT_TYPE == car(value)->type);
-  assert(7 == car(value)->i);
+  assert(7 == ((Integer*)car(value))->value);
 
   value = cdr(value);
   assert(CONS_TYPE == value->type);
   assert(DOUBLE_TYPE == car(value)->type);
-  assert(6 == car(value)->d);
+  assert(6 == ((Double*)car(value))->value);
 
   if (exemplary) {
     value = cdr(value);
     assert(CONS_TYPE == value->type);
     assert(STR_TYPE == car(value)->type);
-    assert(!strcmp("5.0s", car(value)->s));
+    assert(!strcmp("5.0s", ((String*)car(value))->value));
   }
 
   value = cdr(value);
   assert(CONS_TYPE == value->type);
   assert(DOUBLE_TYPE == car(value)->type);
-  assert(4 == car(value)->d);
+  assert(4 == ((Double*)car(value))->value);
 
   if (exemplary) {
     value = cdr(value);
     assert(CONS_TYPE == value->type);
     assert(STR_TYPE == car(value)->type);
-    assert(!strcmp("3.0s", car(value)->s));
+    assert(!strcmp("3.0s", ((String*)car(value))->value));
 
     value = cdr(value);
     assert(CONS_TYPE == value->type);
     assert(STR_TYPE == car(value)->type);
-    assert(!strcmp("2.0s", car(value)->s));
+    assert(!strcmp("2.0s", ((String*)car(value))->value));
   }
 
   value = cdr(value);
   assert(CONS_TYPE == value->type);
   assert(DOUBLE_TYPE == car(value)->type);
-  assert(1 == car(value)->d);
+  assert(1 == ((Double*)car(value))->value);
 
   value = cdr(value);
   assert(NULL_TYPE == value->type);
@@ -110,84 +110,83 @@ int main(int argc, char **argv) {
     exemplary = true;
   }
 
-  Value *head = makeNull();
+  Object *head = makeNull();
   int correctLength = 0;
   assert(length(head) == correctLength);
 
-  Value *reverseLengthZero = reverse(head);
+  Object *reverseLengthZero = reverse(head);
   assert(length(reverseLengthZero) == correctLength);
   cleanup(reverseLengthZero);
 
-  Value *val1 = malloc(sizeof(Value));
+  Integer *val1 = malloc(sizeof(Integer));
   val1->type = INT_TYPE;
-  val1->i = 7;
-  head = cons(val1,head);
+  val1->value = 7;
+  head = cons((Object*)val1,head);
   correctLength++;
   assert(length(head) == correctLength);
 
-  Value *reverseLengthOne = reverse(head);
+  Object *reverseLengthOne = reverse(head);
   assert(length(reverseLengthOne) == correctLength);
   cleanup(reverseLengthOne);
 
-  Value *val2 = malloc(sizeof(Value));
+  Double *val2 = malloc(sizeof(Double));
   val2->type = DOUBLE_TYPE;
-  val2->d = 6.00;
-  head = cons(val2,head);
+  val2->value = 6.00;
+  head = cons((Object*)val2,head);
   correctLength++;
   assert(length(head) == correctLength);
 
   if (exemplary) {
-    Value *val3 = malloc(sizeof(Value));
+    String *val3 = malloc(sizeof(String));
     val3->type = STR_TYPE;
     char *text = "5.0s";
-    val3->s = malloc(sizeof(char)*(strlen(text) + 1));
-    strcpy(val3->s,text);
-    head = cons(val3,head);
+    val3->value = malloc(sizeof(char)*(strlen(text) + 1));
+    strcpy(val3->value,text);
+    head = cons((Object*)val3,head);
     correctLength++;
     assert(length(head) == correctLength);
-
   }
 
-  Value *val4 = malloc(sizeof(Value));
+  Double *val4 = malloc(sizeof(Double));
   val4->type = DOUBLE_TYPE;
-  val4->d = 4.00000;
-  head = cons(val4,head);
+  val4->value = 4.00000;
+  head = cons((Object*)val4,head);
   correctLength++;
   assert(length(head) == correctLength);
 
   if (exemplary) {
-    Value *val5 = malloc(sizeof(Value));
+    String *val5 = malloc(sizeof(String));
     val5->type = STR_TYPE;
     char *text = "3.0s";
-    val5->s = malloc(sizeof(char)*(strlen(text) + 1));
-    strcpy(val5->s,text);
-    head = cons(val5,head);
+    val5->value = malloc(sizeof(char)*(strlen(text) + 1));
+    strcpy(val5->value,text);
+    head = cons((Object*)val5,head);
     correctLength++;
     assert(length(head) == correctLength);
 
-    Value *val6 = malloc(sizeof(Value));
+    String *val6 = malloc(sizeof(String));
     val6->type = STR_TYPE;
     text = "2.0s";
-    val6->s = malloc(sizeof(char)*(strlen(text) + 1));
-    strcpy(val6->s,text);
-    head = cons(val6,head);
+    val6->value = malloc(sizeof(char)*(strlen(text) + 1));
+    strcpy(val6->value,text);
+    head = cons((Object*)val6,head);
     correctLength++;
     assert(length(head) == correctLength);
 
   }
 
-  Value *val7 = malloc(sizeof(Value));
+  Double *val7 = malloc(sizeof(Double));
   val7->type = DOUBLE_TYPE;
-  val7->d = 1.0;
-  head = cons(val7,head);
+  val7->value = 1.0;
+  head = cons((Object*)val7,head);
   correctLength++;
   assert(length(head) == correctLength);
 
-    
+
   display(head);
   testForward(head, correctLength, exemplary);
 
-  Value *rev = reverse(head);
+  Object *rev = reverse(head);
   display(rev);
 
   testBackward(rev, correctLength, exemplary);
